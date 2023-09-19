7d662a6de630ead5786128e3d287670b6c4d57a1
oliviavz <vzolivia@gmail.com>
2023-09-19 15:56:16 -0400

    created query for the prompt: what is _ plus _?

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -17,6 +17,13 @@ export default function QueryProcessor(query: string): string {
       "Olivia");
   }
 
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+
   return "";
 
   
